3c24187cb8f75329c94af60cbc12248b438657d5
rafagabidulin <rafagabidulin@gmail.com>
2022-11-25 21:03:56 +0300

    added Router, store, Route Posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+import Layout from './components/Layout/Layout';
+import Posts from './components/Posts/Posts';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <Provider store={store}>
+          <Layout>
+            <Routes>
+              <Route path='/posts' element={<Posts />} />
+            </Routes>
+          </Layout>
+        </Provider>
+      </BrowserRouter>
     </div>
   );
 }
