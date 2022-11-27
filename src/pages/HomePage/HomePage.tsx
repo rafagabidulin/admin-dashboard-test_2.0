0a77ff132de2cd6aa2c3427e0cd99a5342be7e1b
rafagabidulin <rafagabidulin@gmail.com>
2022-11-27 21:48:16 +0300

    updated HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <Card className='text-center'>
+      <Card.Header />
+      <Card.Body>
+        <Card.Title>Posts Page</Card.Title>
+        <Card.Text>Click on the button to see posts or create a new one</Card.Text>
+        <Link to='posts'>
+          <Button variant='primary'>Go to Posts Page</Button>
+        </Link>
+      </Card.Body>
+      <Card.Footer className='text-muted' />
+      <Card.Body>
+        <Card.Title>Albums Page</Card.Title>
+        <Card.Text>Click on the button to see pictures</Card.Text>
+        <Link to='albums'>
+          <Button variant='primary'>Go to Albums Page</Button>
+        </Link>
+      </Card.Body>
+      <Card.Footer className='text-muted' />
+      <Card.Body>
+        <Card.Title>Todos</Card.Title>
+        <Card.Text>Click on the button to go on the Todos Page</Card.Text>
+        <Link to='todos'>
+          <Button variant='primary'>Go to Todos Page</Button>
+        </Link>
+      </Card.Body>
+      <Card.Footer className='text-muted' />
+    </Card>
+  );
 }
 
 export default HomePage;
